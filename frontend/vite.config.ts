ca657a364f01ef46b24ed8f36a62c8309db522ae
大下慎平 <one1218all38@icloud.com>
2025-07-09 11:11:11 +0900

    npm run format実行

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 5173,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5001",
-				changeOrigin: true,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+    },
+  },
 });
